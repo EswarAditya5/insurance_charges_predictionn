2f0c053e671072060b0b791a2c01f678dc2f9fbb
Eswar Aditya <142793639+EswarAditya5@users.noreply.github.com>
2023-10-07 13:50:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-from predictions import predict
+
 
 # In[2]:
 
@@ -20,21 +20,52 @@ col1,col2,col3=st.columns(3)
 with col1:
     age = st.slider('age',2,100,1)
 with col2:
-    bmi = st.slider('bmi',14,60,4)
+    bmi = st.slider('bmi',1,60,4)
 with col3:
-    children = st.slider('children',0,5,1)
+    children = st.slider('children',0,5,0)
 
 
-# In[3]:
+# In[5]:
+
+insurance=pd.read_csv('insurance.csv')
+y=insurance.charges
+X=insurance.drop('charges',axis=1)
+
+
+# from sklearn.linear_model import LinearRegression
+
+# reg=LinearRegression()
+
+# reg.fit(X,y)
+
+# reg.score(X,y)
+
+import joblib
+
+# joblib.dump(reg,'reg_model.sav')
+
+
+
+from sklearn.tree import DecisionTreeRegressor
+tree=DecisionTreeRegressor(max_depth=16)
+tree.fit(X,y)
+tree.score(X,y)
+
+joblib.dump(tree,'tree_model.sav')
+
+
 
 
 if st.button('Predict Charges'):
-    result=predict(np.array([['age','bmi','children']]))
+    result=tree.predict(np.array([[age,bmi,children]]))
     st.text(result[0])
 
 
-# In[ ]:
+# In[6]:
 
 
 
 
+def predict(data):
+    clf=joblib.load('tree_model.sav')
+    return clf.predict(data)
